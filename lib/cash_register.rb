8773f5d016c62c1cdf3256fdf73245e335e2aa5c
Julie  McManus <julieannesays@gmail.com>
2020-02-04 23:44:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    self.items << title 
+    self.items << title
   end
 
   def apply_discount
@@ -24,4 +24,3 @@ class CashRegister
       end
     end
   end
-
